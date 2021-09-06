e871817862c9995ddec3ced51ca6f75d3ebcdf67
Arunkarthick <arunkarthickk82@gmail.com>
2021-09-06 15:55:45 +0530

    wer

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => '10165464a3e74812b18224168a98642ed9b82b74',
+        'name' => 'arun-karthick-yash/phpcom',
+        'dev' => true,
+    ),
+    'versions' => array(
+        'arun-karthick-yash/phpcom' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => '10165464a3e74812b18224168a98642ed9b82b74',
+            'dev_requirement' => false,
+        ),
+    ),
+);
